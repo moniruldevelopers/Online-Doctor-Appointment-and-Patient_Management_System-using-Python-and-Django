f78802306f9b0c690681e06a8804ff3cc64ff0cb
Monirul Islam <114785562+moniruldevelopers@users.noreply.github.com>
2025-07-26 23:56:38 +0600

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pythonlearn.site@gmail.com'
-EMAIL_HOST_PASSWORD = 'sthv okue tsyg azbu'  
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'passwrod'  
 
 
 # Authentication
